107793d0c9f094a3fccad89d07dc36606e8498a6
Dragna1945 <dragna1945@gmail.com>
2023-05-17 23:36:02 +0300

    2.0

--- .idea/katacalc/src/Main.java
+++ .idea/katacalc/src/Main.java
@@ -3,34 +3,38 @@ import java.util.Arrays;
 import java.lang.Enum;
 import java.util.Scanner;
 public class Main {
-    public static void main (String[] args) {
+    public static void main () {
         System.out.println("Введите выражение");
         Scanner sc = new Scanner(System.in);
+        System.out.println(calc(sc.nextLine()));
+
+    }
+    public static String calc (String input) {
         int a = 0;
         int b = 0;
         String op = "";
-        String [] legsymb ={"1","2","3","4","5","6","7","8","9","10","+","-","/","*","I","II","III","IV","V","VI","VII","VII","IX","X"};
-            String input = sc.next();
-            String[] strings = input.split("[+-/*]");
-            String[] operator = input.split("\\w");
-            if ((Arrays.asList(legsymb).contains(strings[0]))&(Arrays.asList(legsymb).contains(strings[1]))){
+        String rs = "";
+        String[] legsymb = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "+", "-", "/", "*", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] strings = input.split("[+-/*]");
+        String[] operator = input.split("\\w");
+        if ((Arrays.asList(legsymb).contains(strings[0])) & (Arrays.asList(legsymb).contains(strings[1]))) {
             if (strings.length < 3) {
                 op = operator[operator.length - 1];
-                try{
+                try {
                     a = Integer.parseInt(strings[0]);
                     b = Integer.parseInt(strings[1]);
-                } catch (Exception e){
-                   int  r1 = (RomeNumb.valueOf(strings[0]).getAnumb());
-                   int  r2 = (RomeNumb.valueOf(strings[1]).getAnumb());
-                    int res =(operation(r1,r2,op));
-                    if (res<0){
+                } catch (Exception e) {
+                    int r1 = (RomeNumb.valueOf(strings[0]).getAnumb());
+                    int r2 = (RomeNumb.valueOf(strings[1]).getAnumb());
+                    int res = (operation(r1, r2, op));
+                    if (res < 0) {
                         throw new RuntimeException("В римских нет отрицательных чисел");
-                    }else {
-                      System.out.println(RomeNumb.getByValue(res));
+                    } else {
+                        rs=(RomeNumb.getByValue(res));
                     }
                 }
                 if ((a < 11) & (a >= 0) & (b < 11) & (b >= 0)) {
-                    System.out.println(operation(a, b, op));
+                    rs=(operation(a, b, op));
                 } else {
                     throw new RuntimeException("Не разрешенные числа");
 
@@ -38,11 +42,11 @@ public class Main {
             } else {
                 throw new RuntimeException("Более 1 знака операции");
             }
-        } else{
-                throw new RuntimeException("Выражение содержит не разрешенные символы");
+        } else {
+            throw new RuntimeException("Выражение содержит не разрешенные символы");
         }
-     }
-
+        return rs;
+    }
     public static int operation(int a, int b, String op) {
         switch (op) {
             case "*":
--- .idea/katacalc/src/RomeNumb.java
+++ .idea/katacalc/src/RomeNumb.java
@@ -4,9 +4,9 @@ public enum RomeNumb {
     V( 5), VI( 6), VII(7), VIII(8),
     IX( 9), X( 10),
     XI( 11), XII( 12), XIII(13), XIV(14),
-    XVI( 16), XVII(17), XVIII(18),
+    XV( 15), XVI( 16), XVII(17), XVIII(18),
     XIX( 19), XX( 20),
-    XXI( 21), XXII( 22), XXIII(3), XXIV(24),
+    XXI( 21), XXII( 22), XXIII(23), XXIV(24),
     XXV( 25), XXVI( 26), XXVII(27), XXVIII(28),
     XXIX( 29), XXX( 30),
     XXXI( 31), XXXII( 32), XXXIII(33), XXXIV(34),
@@ -56,9 +56,4 @@ public enum RomeNumb {
 
 
 }
-
-
-
-
-
 
